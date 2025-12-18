3ed40312c2b641032ccaa972cc5c8c9a55c120cf
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-18 02:56:04 -0500

    Update hotspot-entrypoint.sh

--- hotspot-entrypoint.sh
+++ hotspot-entrypoint.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env sh
+set -e
+
+# Render provides DATABASE_URL
+# Piped expects JDBC_DATABASE_URL
+if [ -n "$DATABASE_URL" ] && [ -z "$JDBC_DATABASE_URL" ]; then
+  export JDBC_DATABASE_URL="$DATABASE_URL"
+fi
 
 MAX_MEMORY=${MAX_MEMORY:-1G}
 
-java -server -Xmx"$MAX_MEMORY" -XX:+UnlockExperimentalVMOptions -XX:+HeapDumpOnOutOfMemoryError -XX:+OptimizeStringConcat -XX:+UseStringDeduplication -XX:+UseCompressedOops -XX:+UseNUMA -XX:+UseG1GC -jar /app/piped.jar
+exec java \
+  -server \
+  -Xmx"$MAX_MEMORY" \
+  -XX:+UnlockExperimentalVMOptions \
+  -XX:+HeapDumpOnOutOfMemoryError \
+  -XX:+OptimizeStringConcat \
+  -XX:+UseStringDeduplication \
+  -XX:+UseCompressedOops \
+  -XX:+UseNUMA \
+  -XX:+UseG1GC \
+  -jar /app/piped.jar
